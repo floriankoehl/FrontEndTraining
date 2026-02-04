70b50c9da84344927a7807fc774a466998ab5e04
floriankoehl <florian.v.koehl@gmail.com>
2026-02-04 15:37:42 +0100

    groups can be reordered in list and have tasks

--- src/new_start/ListOrder.jsx
+++ src/new_start/ListOrder.jsx
@@ -1,240 +1,266 @@
 import { useEffect, useState } from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
-const INITIAL_TASKS = [
-  "Konzept",
-  "Bar-Plan",
-  "Event-Setup",
-  "Musik",
-  "Finanzen",
-];
+const INITIAL_GROUPS = {
+  Logistik: {
+    tasks: ["Aufbau koordinieren", "Abbau planen", "Materialtransport"]
+  },
+  Gastronomie: 
+  {
+    tasks: [
+    "Getränkebestellung",
+    "Bar-Plan erstellen",
+    "Personal einteilen",
+  ]
+  },
+  Unterhaltung: 
+  {
+    tasks: [
+    "Rahmenprogramm planen",
+    "Moderation organisieren",
+    "Special Acts koordinieren",
+    "Ideen finden",
+    "mehr und mehr"
+  ],
+  },
 
-const TASKWIDTH = 300
+};
+
+const GROUPWIDTH = 400;
+const GROUPHEIGHT = 50;
+const GROUP_COLLAPSE_HEIGHT = 50;
+const GROUP_EXPANDED_HEIGHT_DEFAULT = 200;
 const TASKHEIGHT = 50
 
-const updated_tasks = INITIAL_TASKS.map((task, index)=>{
-    return ({
-        name: task, 
-        position: {
-            x: 0,
-            y: TASKHEIGHT * index, 
-            height: TASKHEIGHT,
-            width: TASKWIDTH
-        }
-    })
-})
+const modified_groups = {};
+const groupKeys = Object.keys(INITIAL_GROUPS);
+for (let i = 0; i < groupKeys.length; i++) {
+  const group_key = groupKeys[i];
 
-const inital_taskOrder = updated_tasks.map((task)=>{
-    return (
-        task.name
-    )
-})
+  modified_groups[group_key] = {
+    collapsed: false,
+    order_number: i,
+    x: 0,
+    y: 0,
+    height: TASKHEIGHT * INITIAL_GROUPS[group_key].tasks.length,
+    width: GROUPWIDTH,
+    tasks: INITIAL_GROUPS[group_key].tasks
+  };
+}
+console.log("This is important", modified_groups);
 
+const inital_groupOrder = Object.keys(INITIAL_GROUPS).map((group) => {
+  return group;
+});
 
+export default function ListOrder() {
+  const [groups, setGroups] = useState(modified_groups);
+  const [groupOrder, setGroupOrder] = useState(inital_groupOrder);
+  const [fromIndex, setFromIndex] = useState(null);
+  const [toIndex, setToIndex] = useState(null);
+  const [rebuildGroups, setRebuildGroups] = useState(0);
 
+  useEffect(() => {
+    const new_groups = {};
+    let accumalative_height = 0;
 
+    // console.log("_____STARTING______")
+    // console.log("_____STARTING______")
+    // console.log("_____STARTING______")
+    // console.log("_____STARTING______")
+    for (let group_index in groupOrder) {
+      const group_key = groupOrder[group_index];
+      const group = groups[group_key];
+      const group_height = group.height;
 
-
-
-
-
-
-
-export default function ListOrder(){
-    const [tasks, setTasks] = useState(updated_tasks)
-    const [taskOrder, setTaskOrder] = useState(inital_taskOrder)
-    const [fromIndex, setFromIndex] = useState(null)
-    const [toIndex, setToIndex] = useState(null)
-
-    // Rerenderes the list when taskOrder changed
-    useEffect(()=>{
-        const new_tasks = taskOrder.map((task, index)=>{
-            return ({
-                name: task, 
-                position: {
-                    x: 0,
-                    y: TASKHEIGHT * index, 
-                    height: TASKHEIGHT,
-                    width: TASKWIDTH
-                }
-            })
-        })
-        console.log("NEW TASKS", new_tasks)
-        setTasks(new_tasks)
-    },[taskOrder])
-
-
-    // Simply swaps positions of elements
-    const change_order = (from_index, to_index) => {
-
-        const copy_order_tasks = [...taskOrder]
-
-        // removing element
-        const [second_element] = copy_order_tasks.splice(from_index,1)
-
-        // adding at correct position again
-        copy_order_tasks.splice(to_index, 0, second_element)
-
-        setTaskOrder(copy_order_tasks)
-
+      new_groups[group_key] = {
+        collapsed: group.collapsed,
+        order_number: group_index,
+        x: 0,
+        y: accumalative_height,
+        height: group_height,
+        width: GROUPWIDTH,
+        tasks: group.tasks
+      };
+      accumalative_height += group_height;
     }
 
-    // Complete Drag and Drop functionality
-    const onMouseDown = (event, current_index) => {
-        const draggedTask = tasks[current_index]
-        const startX = event.clientX - draggedTask.position.x
-        const startY = event.clientY - draggedTask.position.y
-        let y_set_to = draggedTask.position.y
-        let to_index = current_index
+    console.log("Changed", new_groups)
+    setGroups(new_groups);
+  }, [groupOrder, rebuildGroups]);
 
-        // Tell Browser which index is dragged for Z-index
-        setFromIndex(current_index)
+  // Simply swaps positions of elements
+  const change_order = (from_index, to_index) => {
+    const copy_order_groups = [...groupOrder];
 
-        
+    // removing element
+    const [second_element] = copy_order_groups.splice(from_index, 1);
 
+    // adding at correct position again
+    copy_order_groups.splice(to_index, 0, second_element);
 
+    setGroupOrder(copy_order_groups);
+  };
 
+  // Complete Drag and Drop functionality
+  const onMouseDown = (event, key) => {
+    const draggedGroup = groups[key];
+    const current_index = draggedGroup.order_number;
 
+    const startX = event.clientX - draggedGroup.x;
+    const startY = event.clientY - draggedGroup.y;
+    let to_index = current_index;
 
+    setFromIndex(current_index);
 
+    const onMouseMove = (event) => {
+      const new_x = event.clientX - startX;
+      const new_y = event.clientY - startY;
 
+      // Evaluate index for snapping
+      for (let comapre_key in groups) {
+        const compare_group = groups[comapre_key];
 
-
-
-
-        const onMouseMove = (event) => {
-            const new_x = event.clientX - startX
-            const new_y = event.clientY - startY
-
-            
-            // Evaluate index for snapping
-            for (let i in tasks) {
-                const compare_task = tasks[i]
-                // Snaps to index if the current y is +- 20 of the current index y position
-                if (new_y > compare_task.position.y - 20 &&
-                    new_y < compare_task.position.y + 20
-                ) {
-                    console.log("UPADETED", i)
-                    to_index = i
-                }
-            }
-            setToIndex(to_index)
-
-
-
-
-            setTasks((prevTasks)=>{
-                return prevTasks.map((task, compare_index)=>{
-                    if (compare_index !== current_index) {
-                        return task
-                    }
-
-                    return {
-                        ...task, 
-                        position: {
-                            ...task.position,
-                            x: new_x,
-                            y: new_y
-                        }
-                    }
-                })
-            })
+        if (new_y > compare_group.y - 20 && new_y < compare_group.y + 20) {
+          to_index = groups[comapre_key].order_number;
         }
-        
-        
+      }
+      setToIndex(to_index);
+
+      // Update groups while dragging (has nothing to do with snapping)
+      setGroups((prevGroups) => {
+        return {
+          ...prevGroups,
+          [key]: {
+            ...prevGroups[key],
+            x: new_x,
+            y: new_y,
+          },
+        };
+      });
+    };
+
+    const onMouseUp = () => {
+      // Snap to correct position
+      change_order(current_index, to_index);
+      setToIndex(null);
+      setFromIndex(null);
+
+      // Clean up the Event Listeners
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  };
+
+  const change_height = (key) => {
+    setGroups((prev) => {
+      const nextCollapsed = !prev[key].collapsed;
+
+      return {
+        ...prev,
+        [key]: {
+          ...prev[key],
+          collapsed: nextCollapsed,
+          height: nextCollapsed
+            ? GROUP_COLLAPSE_HEIGHT
+            : TASKHEIGHT * prev[key].tasks.length,
+        },
+      };
+    });
+
+    setRebuildGroups((v) => v + 1);
+  };
+
+  return (
+    <>
+      <div className="h-screen w-screen bg-gray-400 p-20">
+        <div className="h-full w-full bg-white relative">
+          {Object.entries(groups).map(([key, value]) => {
+            return (
+              <div
+                className="bg-blue-200 absolute border flex p-1  select-none flex justify-between "
+                onMouseDown={(e) => {
+                  onMouseDown(e, key);
+                }}
+                key={key}
+                style={{
+                  top: value.y,
+                  left: value.x,
+                  width: value.width,
+                  height: value.height,
+                  zIndex: fromIndex == groups[key].order_number ? 10 : 5,
+                  boxShadow:
+                    fromIndex === groups[key].order_number
+                      ? "0 8px 20px rgba(0, 0, 0, 0.62)"
+                      : "none",
+                }}
+              >
+                {toIndex == groups[key].order_number && (
+                  <div className="absolute top-0 w-full bg-black h-2"></div>
+                )}
 
 
-        const onMouseUp = () => {
-
-            // Snap to correct position
-            change_order(current_index, to_index)
-            setToIndex(null)
-            setFromIndex(null)
-
-            // Clean up the Event Listeners
-            document.removeEventListener("mousemove", onMouseMove)
-            document.removeEventListener("mouseup", onMouseUp)
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        document.addEventListener("mousemove", onMouseMove)
-        document.addEventListener("mouseup", onMouseUp)
-    }
-
-
-
-    return (
-        <>
-            <div className="h-screen w-screen bg-gray-400 p-20">
-                {/* <Button 
-                onClick={()=>{change_order(1, 0)}}
-                variant="contained">Change Order</Button> */}
-                <div className="h-full w-full bg-white relative">
-                    {tasks.map((task, index)=>{
-                        return (
-
-                            <div
-                            className="bg-blue-200 absolute border flex items-center  select-none flex justify-between"
-                            
-                            onMouseDown={(e)=>{onMouseDown(e, index)}}
-                            key={index}
-                            style={{
-                                top: task.position.y,
-                                left: task.position.x,
-                                width: task.position.width,
-                                height: task.position.height,
-                                zIndex: fromIndex == index ? 10 : 5,
-                                boxShadow:
-                                            fromIndex === index
-                                            ? "0 8px 20px rgba(0, 0, 0, 0.62)"
-                                            : "none",
-                            }}
-                            >   
-                            {toIndex == index && <div className="absolute top-0 w-full bg-black h-2"></div>} 
-
-                            
-                                {task.name}
-                                <Button 
-                                onClick={()=>{change_order(index, 0)}}
-                                variant="contained">Change Order</Button>
-                            </div>
-                        )
-                    })}
+                <h1 className="font-bold text-lg">{key}</h1>
+                <div className="flex flex-col">
+                {Object.entries(value.tasks).map(([task_key, task_value])=>{
+                    return (
+                        
+                        <div 
+                        style={{
+                            display: value.collapsed ? "none" : "block",
+                            height: `${TASKHEIGHT}px`,
+                            width: "200px"
+                        }}
+                        className="bg-white border"
+                        key={task_key}>
+                        {task_value}
+                        </div>
+                    )
+                })}
                 </div>
-            </div>
-        </>
-    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+                <div className="absolute bottom-0 left-0">
+                  <Button
+                    className="h-[15px]"
+                    onClick={() => {
+                      change_order(groups[key].order_number, 0);
+                    }}
+                    variant="contained"
+                  >
+                    On Top
+                  </Button>
+                  <Button
+                    className="h-[15px]"
+                    onClick={() => {
+                      change_height(key);
+                    }}
+                    variant="contained"
+                    color="success"
+                  >
+                    Resize
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
